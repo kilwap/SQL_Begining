f49bc71e09cad443cd0a516803bb341970c867b5
Piotr Pawlik <59952821+kilwap@users.noreply.github.com>
2020-09-18 11:05:01 +0200

    Add files via upload

--- population_queries_my.sql
+++ population_queries_my.sql
@@ -1,19 +1,36 @@
+-- This is the first query:
+
 SELECT DISTINCT year FROM population_years;
+
+
+-- Populations of Gabon:
 
 SELECT * FROM population_years
 WHERE country = 'Gabon'
 ORDER BY population DESC;
+
+
+-- Smallest 2005 countries by population:
 
 SELECT * FROM population_years
 WHERE year = 2005
 ORDER BY population
 LIMIT 10;
 
+
+-- Countries with >100M population in 2010
+
 SELECT * FROM population_years
 WHERE population > 100 AND year = 2010;
 
+
+-- Countries containing "islands"
+
 SELECT DISTINCT COUNT(country) FROM population_years
 WHERE country LIKE '%Islands%' AND year = 2000;
+
+
+-- Difference in population in Indonesia
 
 SELECT * FROM population_years
 WHERE year = 2000 AND country = 'Indonesia' OR year = 2010 AND country = 'Indonesia';
